6111268fe0d2f27fc33e695c28c0407e0a39fd87
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-08-03 13:19:46 -0400

    Create checkup.php

---
+++ besteventplugin/installation/checkup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The purpose of this file is to validate minimum system requirments are met before installation begins. 
+ **/
+defined('ABSPATH') or exit();
+
+define('BEP_MINIMUM_PHP', '5.4.0');
+define('BEP_MINIMUM_WP_VER', '4.5');
+
+
+
+?>
