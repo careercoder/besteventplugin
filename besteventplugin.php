f2f7478e1579ab78537f4dce77ee56e4790462e9
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-08-03 12:51:23 -0400

    Create besteventplugin.php

---
+++ besteventplugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Best Event Plugin
+ *
+ * @package     besteventplugin
+ * @author      Jody Fitzpatrick
+ * @copyright   2019 BuzzTriad.com
+ * @license     GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Best Event Plugin
+ * Plugin URI:  https://example.com/plugin-name
+ * Description: Create your very own event website.
+ * Version:     1.0.0
+ * Author:      Jody Fitzpatrick
+ * Author URI:  https://example.com
+ * Text Domain: plugin-slug
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+ defined('ABSPATH') or die();
+ 
+ 
