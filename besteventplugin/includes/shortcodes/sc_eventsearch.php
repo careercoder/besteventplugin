4ba8df57e792fe927400942e0c22cf616d93134f
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-08-03 16:13:11 -0400

    Update sc_eventsearch.php

--- besteventplugin/includes/shortcodes/sc_eventsearch.php
+++ besteventplugin/includes/shortcodes/sc_eventsearch.php
@@ -1,1 +1,19 @@
+<?php
+/**
+ * Shortcode: BEP_EVENTSEARCH
+ *
+ * PURPOSE: WILL DISPLAY A SEARCH FORM FOR EVENTS, AND ITS RESULTS.
+ **/
+defined('ABSPATH') or die();
 
+function bep_eventsearch_shortcode( $atts ) {
+  
+   // capture atts...
+   $atts = shortcode_atts( array(
+      'name' => 'world'
+   ), $atts );
+  
+   return 'Hello ' . $a['name'] . !';
+
+
+}
