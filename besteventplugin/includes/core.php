340cdf60f36790ed7ec2779abc4c47b20fe39408
Jody Fitzpatrick <jody.lee.fitzpatrick@gmail.com>
2019-08-03 21:33:56 -0400

    Update core.php

--- besteventplugin/includes/core.php
+++ besteventplugin/includes/core.php
@@ -1,14 +1,13 @@
 <?php
 defined('ABSPATH') or die();
 
-
 class BestEventPluginCore {
  
-  public function launch(){
-      if( is_admin() ){
-          // admin routes
-      } else {
-          // frontend routes
-      }
-  }
+     public function request(){
+      
+      
+     }
+ 
+ 
+ 
 }
